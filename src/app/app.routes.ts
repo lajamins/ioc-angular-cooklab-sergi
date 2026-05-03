09adee33f5eb58dfe37a7c4bb4d0121fda2b8427
Sergi Perez <lajamins@hotmail.com>
2026-05-03 09:31:10 +0200

    feat: implementació de Lazy Loading per al component de preferits

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { CatalegPageComponent } from './pages/cataleg-page/cataleg-page.componen
 import { SearchComponent } from './features/search/search.component';
 import { DetailComponent } from './features/detail/detail.component';
 import { LoginComponent } from './features/login/login.component';
-import { PreferitsPanelComponent } from './components/preferits-panel/preferits-panel.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
@@ -12,6 +11,9 @@ export const routes: Routes = [
   { path: 'cerca', component: SearchComponent },
   { path: 'detall/:id', component: DetailComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'preferits', component: PreferitsPanelComponent, canActivate: [authGuard] },
+  { path: 'preferits',
+    loadComponent: () => import('./components/preferits-panel/preferits-panel.component').then(m => m.PreferitsPanelComponent),
+    canActivate: [authGuard]
+  },
   { path: '**', redirectTo: 'cataleg' }
 ];
